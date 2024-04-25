c48f6b31d902398a60b0d84e414fadd21353420a
Deivideich <david_vl2003@hotmail.com>
2024-04-25 07:34:23 -0600

    Added trajectory node for close loop

--- ws_RAMoncito/src/closed/closed/controller.py
+++ ws_RAMoncito/src/closed/closed/controller.py
@@ -1,7 +1,8 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
-from geometry_msgs.msg import Twist, Pose2D, Point
+from geometry_msgs.msg import Twist, Pose2D, Point, PoseArray, Pose
+from std_msgs.msg import Int16
 import numpy as np
 import math
 
@@ -21,10 +22,11 @@ class Controller(Node):
 
         # Angular velocity subscribers
         self.odom_subscriber = self.create_subscription(Pose2D, 'odom', self.odom_callback, qos_profile=qos_profile_sub) # odometry topic subscriber
-        self.point_subscriber = self.create_subscription(Point, '/trajectory', self.point_callback, qos_profile=qos_profile_sub) # odometry topic subscriber
+        self.point_subscriber = self.create_subscription(PoseArray, '/trajectory', self.get_puntos_callback, 10) # pose topic subscriber
 
         # Velocity publisher
         self.vel_publisher = self.create_publisher(Twist, 'cmd_vel', 10) # velocity topic publisher
+        self.point_publisher = self.create_publisher(Int16, '/number_of_points', 10)
         self.vel_period = 0.05 # velocity publishing period (seconds)
         self.vel_timer = self.create_timer(self.vel_period, self.vel_callback) # velocity publishing timer
         self.msg_vel = Twist() # velocity message
@@ -44,11 +46,6 @@ class Controller(Node):
     #         return polygon
 
     # Variables inicialization
-
-
-    def point_callback(self,msg):
-        for i in range (msg)
-        self.puntos
 
     def variablesInit(self):
         
@@ -80,15 +77,16 @@ class Controller(Node):
         # Flags when angVelocity and Linear Velocity end
         self.vfinish = True
         self.afinish = True
+        self.finish_config = False
 
         # How many points have been crossed
         self.count = 0
 
         # Points
-        self.puntos = self.generate_polygon(4,0.3)        
+        self.puntos = []     
         
-        self.xT = self.puntos[self.count][0]
-        self.yT = self.puntos[self.count][1]
+        self.xT = 0
+        self.yT = 0
 
         self.TARGET_TOLERANCE = 0.02
 
@@ -99,9 +97,18 @@ class Controller(Node):
         self.minlinear = 0.05
         self.minang = 0.4
 
+    def get_puntos_callback(self,msg):
+        if(self.finish_config == False):
+            for pose in msg.poses:
+                self.punto = []
+                self.punto.append(pose.position.x)
+                self.punto.append(pose.position.y)
+                self.puntos.append(self.punto)
+            
+            self.finish_config = True
+
 
     def getVelocity(self):
-
         # After one part of the trajectory is ready, update points
         if self.vfinish and self.afinish:
             self.distance = 0
@@ -147,54 +154,7 @@ class Controller(Node):
                 self.vfinish = True
         else:
             self.v = 0.0
-
-        #if self.finish:
-        #    self.v = 0.0
-        #    self.w = 0.0
-
-        # self.distance_t = math.sqrt((self.xT - self.x)**2 + (self.yT - self.y)**2)
-        
-        # deltay = self.yT - self.y
-        # deltax = self.xT - self.x
-        
-        # # abs_delta_y = abs(self.yT) - abs(self.y)
-        # # abs_delta_x = abs(self.xT) - abs(self.x)
-
-        # if (self.distance_t < self.TARGET_TOLERANCE):
-        #     self.count+=1
-        #     if self.count >= len(self.puntos):
-        #         self.count=0
-        #     self.xT = self.puntos[self.count][0]
-        #     self.yT = self.puntos[self.count][1]
-        
-
-        # # self.distance = np.sqrt((self.x)**2 + (self.y)**2)
-        
-        # self.error_d = np.sqrt((deltax)**2 + (deltay)**2)
-        # self.thetaT = np.arctan2(deltay, deltax)
-        # errorDer = 0
-        
-        # #Obtain angular error and its complementary error, and see which is the lowest
-        # errorIzq = self.thetaT * self.theta 
-        # if(errorIzq < 0): errorDer = errorIzq + 2*np.pi
-        # elif(errorIzq > 0): errorDer = errorIzq - 2*np.pi
-
-        # if(abs(errorIzq) < abs(errorDer)): self.error_ang = errorIzq
-        # else: self.error_ang = errorDer
-
-        # if self.error_ang > 0.05:
-        #     self.w = self.Kw * self.error_ang
-        #     self.v = 0.0
-        # else:
-        #     self.v = self.Kv * self.distance_t
-        #     self.w = 0.0
-
-
-
-        # #Publish velocities
-        
-        
-        
+    
         if abs(self.v) > self.maxlinear:
             self.msg_vel.linear.x = (abs(self.v) / self.v) * self.maxlinear
         elif 0.0 < abs(self.v) and self.v < self.minlinear:
@@ -211,28 +171,28 @@ class Controller(Node):
 
     # Odometry callback
     def vel_callback(self):
+        if self.finish_config:
+            self.getVelocity()
+            self.vel_publisher.publish(self.msg_vel)
+            
 
-        self.getVelocity()
-        self.vel_publisher.publish(self.msg_vel)
-        
-
-        # print(self.msg_vel, end='')
-        # print(self.distance, end='')
-        # print(self.theta, end='')
-        # print(self.puntos[self.count], end='')
-        # print(self.thetaT, end='')
-        print("Distance error: ", self.error_d)
-        print("Angle error: ", self.error_ang)
-        print("Local car distance: ",self.distance)
-        print("Target distance: ", self.distance_t)
-        print("Difference between local car distance and target distance: ", self.distance_t-self.distance)
-        # self.get_logger().info('Velocities: {}'.format(self.msg_vel))
-        # self.get_logger().info('Distance: {}'.format(self.distance))
-        # self.get_logger().info('Angle: {}'.format(self.theta))
-        #self.get_logger().info('Punto: {}'.format(self.puntos[self.count]))
-        # self.get_logger().info('theta: {}'.format(self.thetaT))
-        # self.get_logger().info('error_d: {}'.format(self.error_d))
-        # self.get_logger().info('error_a: {}'.format(self.error_ang))
+            # print(self.msg_vel, end='')
+            # print(self.distance, end='')
+            # print(self.theta, end='')
+            # print(self.puntos[self.count], end='')
+            # print(self.thetaT, end='')
+            print("Distance error: ", self.error_d)
+            print("Angle error: ", self.error_ang)
+            print("Local car distance: ",self.distance)
+            print("Target distance: ", self.distance_t)
+            print("Difference between local car distance and target distance: ", self.distance_t-self.distance)
+            # self.get_logger().info('Velocities: {}'.format(self.msg_vel))
+            # self.get_logger().info('Distance: {}'.format(self.distance))
+            # self.get_logger().info('Angle: {}'.format(self.theta))
+            #self.get_logger().info('Punto: {}'.format(self.puntos[self.count]))
+            # self.get_logger().info('theta: {}'.format(self.thetaT))
+            # self.get_logger().info('error_d: {}'.format(self.error_d))
+            # self.get_logger().info('error_a: {}'.format(self.error_ang))
 
     # Update left motor angular velocity
     def odom_callback(self, msg):
--- ws_RAMoncito/src/closed/closed/trajectory_generator.py
+++ ws_RAMoncito/src/closed/closed/trajectory_generator.py
@@ -1,34 +1,33 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import PoseArray, Pose
 from std_msgs.msg import Int32
 import numpy as np
 
 class TrajectoryGenerator(Node):
     def __init__(self):
         super().__init__('trajectory_generator')
-        self.subscription = self.create_subscription(Int32,'/number_of_points',self.number_of_points_callback,10)
-        self.publisher = self.create_publisher(Point, '/trajectory', 10)
-
-    def publish_point_callback(self, trajectory):
-        for point in trajectory:
-            self.publisher.publish(point)
+        self.subscription = self.create_subscription(Int32, '/number_of_points', self.number_of_points_callback, 10)
+        self.publisher = self.create_publisher(PoseArray, '/trajectory', 10)
+        self.diameter = 0.3
 
     def number_of_points_callback(self, msg):
         num_points = msg.data
-        trajectory = []
+        trajectory = PoseArray()
+        trajectory.header.frame_id = 'map'  # Assuming the trajectory is defined in the 'map' frame
 
         # Generate points for the polygon trajectory
         for i in range(num_points):
-            point = Point()
-            # Generate points on a regular polygon with radius 1 meter
-            point.x = 1.0 * np.cos(2 * np.pi * i / num_points)
-            point.y = 1.0 * np.sin(2 * np.pi * i / num_points)
-            point.z = 0.0  # Assuming 2D trajectory, so z-coordinate is 0
-            trajectory.append(point)
+            pose = Pose()
+            # Generate poses on a regular polygon with radius 1 meter
+            pose.position.x = self.diameter * np.cos(2 * np.pi * i / num_points)
+            pose.position.y = self.diameter* np.sin(2 * np.pi * i / num_points)
+            pose.position.z = 0.0  # Assuming 2D trajectory, so z-coordinate is 0
+            pose.orientation.w = 1.0  # Assuming no rotation
+            trajectory.poses.append(pose)
 
-        # Publish the trajectory points to the '/trajectory' topic
-        self.publish_point_callback(trajectory)
+        # Publish the trajectory as a PoseArray
+        self.publisher.publish(trajectory)
 
 def main(args=None):
     rclpy.init(args=args)
