c027b5fe0ae4cd1cd939cc5996640a39fa6b68bd
JuanCarlosHR2003 <105697745+JuanCarlosHR2003@users.noreply.github.com>
2024-05-23 19:25:04 -0600

    aruco&calibCam comments

--- vision_challenge04/aruco.py
+++ vision_challenge04/aruco.py
@@ -1,14 +1,14 @@
 
-import cv2 # Import the OpenCV library
-import numpy as np # Import Numpy library
+import cv2
+import numpy as np 
 from scipy.spatial.transform import Rotation as R
-import math # Math library
+import math 
 import os
  
-# Dictionary that was used to generate the ArUco marker
+# Nombre del aruco utilizado
 aruco_dictionary_name = "DICT_4X4_50"
  
-# The different ArUco dictionaries built into the OpenCV library. 
+# Diccionario con los diferentes arucos que se pueden utilizar
 ARUCO_DICT = {
   "DICT_4X4_50": cv2.aruco.DICT_4X4_50,
   "DICT_4X4_100": cv2.aruco.DICT_4X4_100,
@@ -29,17 +29,12 @@ ARUCO_DICT = {
   "DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL
 }
  
-# Side length of the ArUco marker in cm 
+# Longitud del aruco en cm
 aruco_marker_side_length = 6
  
-# Calibration parameters yaml file 
+# Función para convertir ángulos en cuaterniones a ángulos en Euler (Roll, Pitch, Yaw)
 def euler_from_quaternion(x, y, z, w):
-  """
-  Convert a quaternion into euler angles (roll, pitch, yaw)
-  roll is rotation around x in radians (counterclockwise)
-  pitch is rotation around y in radians (counterclockwise)
-  yaw is rotation around z in radians (counterclockwise)
-  """
+
   t0 = +2.0 * (w * x + y * z)
   t1 = +1.0 - 2.0 * (x * x + y * y)
   roll_x = math.atan2(t0, t1)
@@ -53,17 +48,14 @@ def euler_from_quaternion(x, y, z, w):
   t4 = +1.0 - 2.0 * (y * y + z * z)
   yaw_z = math.atan2(t3, t4)
       
-  return roll_x, pitch_y, yaw_z # in radians
+  return roll_x, pitch_y, yaw_z # en radianes
  
 def main():
-  """
-  Main method of the program.
-  """
-  # Check that we have a valid ArUco marker
+  # Checar que el Aruco es válido
   if ARUCO_DICT.get(aruco_dictionary_name, None) is None:
-    print("ArUCo tag is not supported")
+    print("No es posible usar ArUco")
  
-  # Load the camera parameters from the saved file
+  # Cargar parámetros de la cámara obtenidos anteriormente
   root = os.getcwd()
   calibrationDir = os.path.join(root,'calibration.npz')
   calib_data = np.load(calibrationDir)
@@ -72,71 +64,68 @@ def main():
   tvecs = calib_data['tvecs']
   rvecs = calib_data['rvecs']
      
-  # Load the ArUco dictionary
-  print("[INFO] detecting '{}' markers...".format(
-    aruco_dictionary_name))
+  # Cargar el diccionario de ArUco
+  print("Detectando '{}' marcador...".format(aruco_dictionary_name))
   this_aruco_dictionary = cv2.aruco.getPredefinedDictionary(ARUCO_DICT[aruco_dictionary_name])
   this_aruco_parameters = cv2.aruco.DetectorParameters()
   detector = cv2.aruco.ArucoDetector(this_aruco_dictionary,this_aruco_parameters)
    
-  # Start the video stream
+  # Comenzar el video stream
   cap = cv2.VideoCapture(0)
    
   while(True):
-  
-    # Capture frame-by-frame
-    # This method returns True/False as well
-    # as the video frame.
     ret, frame = cap.read()  
+    
+    if not ret:
+      break
      
-    # Detect ArUco markers in the video frame
+    # Detecta marcadores de ArUco
     (corners, marker_ids, rejected) = detector.detectMarkers(frame)
        
-    # Check that at least one ArUco marker was detected
+    # Checa que al menos un marcador ha sido detectado
     if marker_ids is not None:
  
-      # Draw a square around detected markers in the video frame
+      # Dibuja un cuadrado alrededor de los marcadores detectados
       cv2.aruco.drawDetectedMarkers(frame, corners, marker_ids)
        
-      # Get the rotation and translation vectors
+      # Se obtiene el vector de translación y matriz de rotación del ArUco con respecto a la cámara
       rvecs, tvecs, obj_points = cv2.aruco.estimatePoseSingleMarkers(
         corners,
         aruco_marker_side_length,
         camMatrix,
         distCoeff)
          
-      # Print the pose for the ArUco marker
-      # The pose of the marker is with respect to the camera lens frame.
-      # Imagine you are looking through the camera viewfinder, 
-      # the camera lens frame's:
-      # x-axis points to the right
-      # y-axis points straight down towards your toes
-      # z-axis points straight ahead away from your eye, out of the camera
+      # Se imprime la pose del ArUco, la cual es con respecto al marco de referencia de la cámara usada
+      # Eje x es positivo hacia la derecha
+      # Eje y es positivo hacia abajo
+      # Eje z es positivo desde la cámara. Profundidad del objeto
       for i, marker_id in enumerate(marker_ids):
        
-        # # Store the translation (i.e. position) information
-        transform_translation_x = tvecs[i][0][0]
-        transform_translation_y = tvecs[i][0][1]
-        transform_translation_z = tvecs[i][0][2]
+        # Desglosar vector de translación en sus tres componentes del primer marcador
+        transform_translation_x = tvecs[0][0][0]
+        transform_translation_y = tvecs[0][0][1]
+        transform_translation_z = tvecs[0][0][2]
  
-        # # Store the rotation information
+        # Guardar la matriz de rotación del primer marcador
         # rotation_matrix = np.eye(4)
-        # rotation_matrix[0:3, 0:3] = cv2.Rodrigues(np.array(rvecs[i][0]))[0]
+        # rotation_matrix[0:3, 0:3] = cv2.Rodrigues(np.array(rvecs[0][0]))[0]
         # r = R.from_matrix(rotation_matrix[0:3, 0:3])
         # quat = r.as_quat()   
          
-        # # Quaternion format     
+        # Formato de cuaternion
         # transform_rotation_x = quat[0] 
         # transform_rotation_y = quat[1] 
         # transform_rotation_z = quat[2] 
         # transform_rotation_w = quat[3] 
          
-        # # Euler angle format in radians
+        # Formato de ángulo de Euler
         # roll_x, pitch_y, yaw_z = euler_from_quaternion(transform_rotation_x, 
         #                                                transform_rotation_y, 
         #                                                transform_rotation_z, 
         #                                                transform_rotation_w)
          
+
+        # Imprimir pose del ArUco
         # roll_x = math.degrees(roll_x)
         # pitch_y = math.degrees(pitch_y)
         # yaw_z = math.degrees(yaw_z)
@@ -148,17 +137,15 @@ def main():
         # print("yaw_z: {}".format(yaw_z))
         print()
         
-        # Draw the axes on the marker
-        cv2.drawFrameAxes(frame, camMatrix, distCoeff, rvecs[i], tvecs[i], aruco_marker_side_length/2, 2)
+        # Dibuja ejes en el marcador
+        cv2.drawFrameAxes(frame, camMatrix, distCoeff, rvecs[0], tvecs[0], aruco_marker_side_length/2, 2)
      
-    # Display the resulting frame
+    # Observar resultados
     cv2.imshow('frame',frame)
-    # If "q" is pressed on the keyboard, 
-    # exit this loop
-    if cv2.waitKey(1) & 0xFF==ord('1'):
+    if cv2.waitKey(20) & 0xFF==ord('1'):
       break
   
-  # Close down the video stream
+  # Cerrar video stream
   cap.release()
   cv2.destroyAllWindows()
    
--- vision_challenge04/cameraCalibration.py
+++ vision_challenge04/cameraCalibration.py
@@ -3,61 +3,107 @@ import numpy as np
 import glob
 import os
 
+# Función para obtener 10 fotos
 def getPics(calibrationDir):
+    # Se abre la cámara a usar
     cap = cv2.VideoCapture(0)
     cnt = 0
     while True:
+        #Se lee el frame de la cámara
         ret, frame = cap.read()
+
+        #Cuando se toman 10 imagenes o no se obtiene un frame de la cámara, entonces
+        # 
         if not ret or cnt >= 10:
             break
+
+        #Se muestra el frame 
         cv2.imshow('camera', frame)
+
+        #Se obtiene el path para guardar la imagen
         img_name = os.path.join(calibrationDir, f'calibrationImg_{cnt}.jpg')
+
+        #Si se presiona 1, se guarda la imagen en el path
         if cv2.waitKey(1) & 0xFF==ord('1'):
             cv2.imwrite(img_name, frame)
             cv2.imshow('Captured img', frame)
             cnt += 1
+
+    #Cerrar video stream
     cap.release()
     cv2.destroyAllWindows()
 
+#Función para calibrar cámara
 def calibrate(showPics=True, takePic=False):
+    #Se obtiene carpeta donde se guardan las imágenes usadas para calibrar cámara
     root = os.getcwd()
     calibrationDir = os.path.join(root,'calibrationImg')
     
+    # Si se desea tomar fotografias, se realiza esta función
     if takePic:
         getPics(calibrationDir)
 
+    #Se obtiene la lista de imágenes a usar para la calibración
     imgPathList = glob.glob(os.path.join(calibrationDir, 'calibrationImg_*.jpg'))
 
-    nRows = 8
-    nCols = 4
-    square_size = 0.9 #cm
+    # Variables del objeto y del mundo real.
+
+    #Numero de filas y columnas del chessboard
+    row = 10
+    col = 8
+
+    #Filas y columnas interiores del chessboard
+    nRows = row - 1
+    nCols = col - 1
+
+    #Tamaño de un cuadrado
+    square_size = 2.3 #cm
+
+    #Criterios para encontrar puntos
     termCriteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    #Puntos del mundo escalados a cm
     worldPtsCur = np.zeros((nRows*nCols, 3), np.float32)
     worldPtsCur[:,:2] = np.mgrid[0:nRows, 0:nCols].T.reshape(-1,2)
     worldPtsCur = worldPtsCur*square_size
+
     worldPtsList = []
     imgPtsList = []
+
+    #Para cada imagen, se encuentran puntos en el chessboard
     for curImgPath in imgPathList:
+        #Preprocesamiento de imagen
         imgBGR = cv2.imread(curImgPath)
         imgGray = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2GRAY)
+
+        #Encontrar puntos en el Chessboard
         cornersFound, cornersOrg = cv2.findChessboardCorners(imgGray, (nRows,nCols), None)
-        cv2.waitKey(500)
+
+        #Si se encuentran puntos
         if cornersFound:
+
+            #Se guardan los puntos en el mundo real
             worldPtsList.append(worldPtsCur)
+            #Se obtienen puntos más exactos con la siguiente función, y se guardan
             cornersRefined = cv2.cornerSubPix(imgGray, cornersOrg, (11,11), (-1,-1), termCriteria)
             imgPtsList.append(cornersRefined)
             if showPics:
+                #Se muestran los puntos de Chessboard encontrados 
                 cv2.drawChessboardCorners(imgBGR, (nRows, nCols), cornersRefined, cornersFound)
                 cv2.imshow('Chessboard', imgBGR)
                 cv2.waitKey(500)
+    
     cv2.destroyAllWindows()
 
+    #Se obtiene la matriz de la cámara, los coeficientes de distorción, la matriz de rotación y el vector de translación
+    # a partir de los puntos en el mundo real y los puntos en la imagen
     repError, camMatrix, distCoeff, rvecs, tvecs = cv2.calibrateCamera(worldPtsList, imgPtsList, imgGray.shape[::-1], None, None)
     print('Camera matrix: \n', camMatrix)
     print('reproj error (px): {:.4f}'.format(repError))
 
+    # Se guardan los datos de calibración en un archivo .npz
     curFolder = os.path.dirname(os.path.abspath(__file__))
-    paramPath = os.path.join(curFolder, 'calibration.npz')
+    paramPath = os.path.join(curFolder, 'calibration_webcam.npz')
     np.savez(paramPath,
              repError=repError,
              camMatrix=camMatrix,
@@ -66,4 +112,6 @@ def calibrate(showPics=True, takePic=False):
              tvecs=tvecs)
 
 if __name__ == '__main__':
+    #Si takePic es verdadero, se capturan 10 imagenes para calibrar la cámara y se guarda en la carpeta calibrationImg
+    #Sino, se obtienen las imagenes de la carpeta calibrationImg para calibrar la cámara
     calibrate(takePic=False)
